44a4643f5b0335eeb283b5476faeede30ded3170
purple-prince <himynameischuk44@gmail.com>
2023-12-27 20:56:56 -0600

    add more params for shape and some calculations

--- Blobby/Blob.swift
+++ Blobby/Blob.swift
@@ -7,11 +7,17 @@ struct Blob: Shape {
     let boo: Bool
     let sides: Double
     let height: Double
+    let randomness: Double
+    let density: Double
     
-    init(boo: Bool, sides: Double, height: Double) {
+    var points: [CGPoint] = []
+    
+    init(boo: Bool, sides: Double, height: Double, randomness: Double, density: Double) {
         self.boo = boo
         self.sides = sides
         self.height = height
+        self.randomness = randomness
+        self.density = density
     }
     
     func path(in rect: CGRect) -> Path {
@@ -20,6 +26,26 @@ struct Blob: Shape {
         let height = rect.height
         
         var points: [CGPoint] = []
+        
+        
+        for angle in stride(from: 0.0, to: 360, by: 360 / sides) {
+            
+            let randomFactor = Double.random(in: 1 - randomness..<1 + randomness)
+            let modifier1 = sin(angle + 90)
+            let modifier2 = cos(angle + 180)
+            let sinCalc = (sin(Double(angle * density) + modifier1 + modifier2)  * randomFactor)
+            
+            points.append(CGPoint(x: 0, y: 0))
+        }
+        
+        let first = points.removeFirst()
+        path.move(to: first)
+        
+        
+        for point in points {
+            path.addLine(to: point)
+        }
+        
         
         return path
     }
@@ -32,6 +58,8 @@ struct Blob: Shape {
         return (x, y)
     }
 }
+
+
 
 
 
--- Blobby/Blobby.swift
+++ Blobby/Blobby.swift
@@ -4,14 +4,19 @@ struct Blobby: View {
     
     @State var boo: Bool = false
     let segments: Double = 360
-    let height: Double = 1.0 // 1.0 gives normal height, heigher height = more amlitude
+    let height: Double = 1.0 // 1.0 gives normal height, heigher height = more amplitude
+    let randomness: Double = 0.0000001//1.0 // 0.001
+    let density: Double = 0.245 // 1.0 gives normal density, lower value gives less density
     
     var body: some View {
         ZStack {
             Color.white.ignoresSafeArea()
                 .brightness(-0.1)
             
-            Blob(boo: boo, sides: segments, height: height)
+            Blob(boo: boo, sides: segments, height: height, randomness: randomness, density: density)
+                .stroke(Color.red, lineWidth: 1.25)
+                    .drawingGroup()
+                    .frame(width: 200, height: 200)
         }
         .onTapGesture { boo.toggle() }
         
